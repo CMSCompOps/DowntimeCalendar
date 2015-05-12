9a16c40c78d86e1970bac2f1e0f6a1a8219a5d06
Mehmet <mehmet3@gmail.com>
2015-05-12 14:40:33 +0200

    add timewindow parameter

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+cd $(dirname $(readlink -f "${BASH_SOURCE[0]}"))
+
 # json key for the google api
-jsonKey="cms-test-49641fc90d34.json"
+jsonKey="DowntimeCalendarsV2-6de026a7257d.json"
 # calendar-CMS site tier map file
 map="calendarTierMap.input"
 # calendar source
-sourceURL="http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=121&batch=1&view=default"
+sourceURL="http://dashb-ssb.cern.ch/dashboard/request.py/getplotdata?columnid=121&time=336&batch=1"
 
 python main.py --key $jsonKey --map $map --source $sourceURL
